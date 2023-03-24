dbf817293b52b35a19d3e2446f30f128d51b023c
chanmin Lee <dlcksals87@gmail.com>
2023-03-24 09:28:25 +0900

    what_is_logical_operators

---
+++ c_chap_1/what_is_logical_operators.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 3;
+    int b = 5;
+    int c = 7;
+    
+    int result1 = (a == 3 && b == 5);  // ??? why not?? 
+    printf(result1);
+   
+
+    // short-circuit 예제
+    
+}
