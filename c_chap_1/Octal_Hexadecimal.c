d3d364c34e7fbd0ba3e599687943ec7f918cab60
chanmin Lee <dlcksals87@gmail.com>
2023-03-27 15:06:36 +0900

    study octal & hexadecimal number in c

---
+++ c_chap_1/Octal_Hexadecimal.c
@@ -0,0 +1,14 @@
+#include <stdio.h>  
+
+int main(void)
+{
+    int num_10 = 15;
+    int Oc_num = 012;
+    int Hex_num = 0x2A;
+    
+    printf("decimal num %d \n", num_10);
+    printf("octal num 012 %d \n", Oc_num);
+    printf("hexadecimal num %d ", Hex_num);
+
+    return 0;
+}
