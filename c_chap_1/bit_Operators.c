9c0c1a6bcd7d57563f2844315ea57f94dbe5a72a
chanmin Lee <dlcksals87@gmail.com>
2023-03-27 22:27:58 +0900

    modify operator ( ^ >> |)

--- c_chap_1/bit_Operators.c
+++ c_chap_1/bit_Operators.c
@@ -2,12 +2,12 @@
 
 int main(void)
 {
-    int num1 = 15;
-    int num2 = 20;
+    int num1 = 15; // 00001111
+    int num2 = 20; // 00010100
     int num3 = num1<<3; // 비트의 shift를 실행한다. <<는 왼쪽으로, >>는 오른쪽으로 이동시킨다. 
 
-    printf("%d \n", num1 & num2); // &연산자는 비트 사이의 논리곱을 실행한다
-    printf("%d \n", num1 ^ num2); // ^연산자는 비트 사이의 논리합을 실행한다
+    printf("%d \n", num1 & num2); // & 연산자는 비트 사이의 논리곱을 실행한다
+    printf("%d \n", num1 | num2); // | 연산자는 비트 사이의 논리합을 실행한다
 
     printf("%d \n", ~num1);       // ~연산자는 비트 단위로 부정형을 가진다. 0 > 1 , 1 > 0
     printf("%d \n", num3);                                                                       
