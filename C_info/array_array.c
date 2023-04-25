3d528d8ddcea881eb436e6cbe7e2e228b89c4ddd
chanmin Lee <dlcksals87@gmail.com>
2023-04-25 15:54:27 +0900

    2-demention array printing

---
+++ C_info/array_array.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#define column 10
+#define row 10
+
+
+int main(void)
+{
+    int array[column][row];
+
+    int i = 0, j = 0;
+    char c = 'A';
+
+    for (i = 0; i < column; i++)
+    {
+        for( j = 0; j < row; j++)
+        {
+            while (c >= 'Z' && c <= '`')
+            {
+                c++;
+            }
+
+            array[i][j] = c;
+            c++;
+
+            if((c) == 'z')
+            {
+                //printf("good");
+                c = 'A';
+            }
+        }
+        
+    }
+
+    for (i = 0; i < column; i++)
+    {
+        for( j = 0; j < row; j++)
+        {
+            printf("%c", array[i][j]); 
+        }
+        printf("\n");
+    }
+    
+
+
+    
+
+}
