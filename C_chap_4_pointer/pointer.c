45ef371d49d3a02e7794976f2675364167a43cd0
chanmin Lee <dlcksals87@gmail.com>
2023-04-28 12:05:30 +0900

    what is *pointer, &pointer and Its manipulating

--- C_chap_4_pointer/pointer.c
+++ C_chap_4_pointer/pointer.c
@@ -10,6 +10,19 @@ int main(void)
     printf("%d\n",pointer_num); 
     // pointer 변수의 주소값 출력 (=&pointer_num)
     printf("%d\n",&pointer_num);
+    // pointer 변수의 주소값에 저장된 변수값 출력 (=num)
+    printf("%d\n",*pointer_num);
+
+    // pointer 변수의 조작 (포인터로 저장한 주소의 변수의 값 변경)
+    // 포인터변수에 *를 붙이면 변수를 참조한 결과임.(=num)
+    (*pointer_num)++; // 1. 포인터변수에 *붙이고 ++한 결과
+    printf("%d", *pointer_num); // 저장된 주소값의 변수에 ++가 됨 (=11)
+
+    *(pointer_num++); // 2. 포인터변수의 주소값에 ++한 결과
+    printf("%d", *pointer_num); // 메모리상 다음 주소값에 저장된 값(쓰레기 값) 출력
+
+    *pointer_num++;   // 우선순위 확인. (1,2) 
+    printf("%d", *pointer_num); // 우선순위 ++ > *, 그래서 이건 2의 결과와 같음.
 
     return 0;
 }
