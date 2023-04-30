4b0f1cc2cc9cf603cf147d17d16ad6f1b4925c8e
chanmin Lee <dlcksals87@gmail.com>
2023-04-30 19:51:02 +0900

    #2 in baekjoon step1

---
+++ C_baekjoon/step_by_step/step_1_input_output/#2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num1 = 0, num2 = 0;
+    
+    scanf("%d", &num1);
+    scanf("%d", &num2);
+    
+    printf("%d", num1+num2);
+    
+    return 0;
+}
