1996bd99622cc7be7065ea67e0720e8d9d59f982
chanmin Lee <dlcksals87@gmail.com>
2023-05-12 21:58:18 +0900

    nomal bag first access

---
+++ C_baekjoon/speed_up/nomal_bag.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#define MAX 100
+
+typedef struct nomal_bag bag;
+struct bag
+{
+    int weight;
+    int values;
+};
+
+
+int main(void)
+{
+    int number = 0, keep = 0;
+    scanf("%d, %d", &number, &keep);
+
+    for(int i = 0; i < number; i++)
+    {
+        
+    }
+}
