e28b0b80964e0b0aaeec622a7a901709f1ba839e
chanmin Lee <dlcksals87@gmail.com>
2023-06-15 23:15:19 +0900

    string array argument in function.

--- C_chap_5_function_with_pointer/fn_array_argument.c
+++ C_chap_5_function_with_pointer/fn_array_argument.c
@@ -9,11 +9,24 @@ void print_int_array(int * array, int len_of_array) // 배열을 매개변수로
     
     return;
 }
+
+void print_str_array(char* * array, int len_of_array)
+{
+    for (int i = 0; i < len_of_array; i++)
+    {
+        printf("%s \n", array[i]);
+    }
+    
+    return;
+}
+
 int main(void)
 {
     int array[] = {10 , 20 , 30};
+    char* string[] = {"apple", "banana", "kiwi"};
 
-    print_int_array(array, sizeof(array)); // 배열을 매개변수로 사용하려면 이처럼 배열의 주소값(이름) 변수를 전달해 주어야 한다. 
+    print_int_array(array, sizeof(array)/sizeof(array[0])); // 배열을 매개변수로 사용하려면 이처럼 배열의 주소값(이름) 변수를 전달해 주어야 한다. 
+    print_str_array(string, sizeof(string)/sizeof(string[0])); // 배열의 크기 = 배열 자체 크기 / 배열의 원소 자료형 크기
+
     return 0;
-
 }
