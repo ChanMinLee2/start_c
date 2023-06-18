ee70cb9607c6cea49b6a3c43ab6a285aa4f07a73
chanmin Lee <dlcksals87@gmail.com>
2023-06-18 20:35:49 +0900

    add function of swap with call of reference

--- C_chap_5_function_with_pointer/call_by_val_or_ref.c
+++ C_chap_5_function_with_pointer/call_by_val_or_ref.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 
-void swap(int a, int b)
+void swap(int a, int b) // 단순히 값의 복사만 일어남, 이 매개변수는 함수종료와 동시에 사라짐.
 {
     int temp = a;
     a = b;
     b = temp;
-    printf("num1 : %d, num2 : %d ", a, b);
+    printf("swap num1 : %d, num2 : %d ", a, b);
+}
+
+void reference_swap(int* a, int *b) // 주소값을 전달받아서 사용함. 
+{
+    int* temp;
+
+    temp = a;
+    a = b;
+    b = a;
+    printf("reference_swap num1 : %d, num2 : %d ", *a, *b); // 주소값 변수의 주소를 사용하려면 *를 붙여 값을 참조해야함.
 }
 
 int main(void)
@@ -14,6 +24,9 @@ int main(void)
     int num2 = 20;
 
     swap(num1, num2);
+    printf("num1 : %d, num2 : %d ", num1, num2);
+
+    reference_swap(&num1, &num2);
     printf("num1 : %d, num2 : %d ", num1, num2);
 
 }
