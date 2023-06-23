ee8089f7e30995cc7e426bed0b67a969b8755a46
chanmin Lee <dlcksals87@gmail.com>
2023-06-23 10:32:09 +0900

    access to 2dimen array

--- C_chap_6_multidimention/array.c
+++ C_chap_6_multidimention/array.c
@@ -2,7 +2,27 @@
 
 int main(void)
 {
-    int villa[4][2];
+    int villa[4][4];
     int popu, i, j;
+
+    for(i=0; i<4; i++)
+    {
+        for(j=0; j<4; j++)
+        {
+            printf("%d類 %d�� 檣掘熱 : ", i+1, j+1);
+            scanf("%d", &villa[i][j]);
+        }
+    }
+
+    for(i=0; i<4; i++)
+    {
+        popu = 0;
+        for(j=0; j<4; j++)
+        {
+            popu += villa[i][j];
+        }
+        printf("%d類 檣掘熱 : %d \n", i+1, popu);
+    }
+
     return 0;
 }
