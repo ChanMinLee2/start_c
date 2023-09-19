9d005fdc2d134305a95ea29baec9721986dd8f14
chanmin Lee <dlcksals87@gmail.com>
2023-09-19 23:14:55 +0900

    alphabet checking in string

---
+++ C_baekjoon/step_by_step/step_5_string/alpha_check.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    int alphacount[26];
+    for(int s = 0; s < 26; s++)
+    {
+        alphacount[s] = -1;
+    }
+    
+    char temp[100];
+    scanf("%s", temp);
+    
+    for(int i = 0; i < strlen(temp)-1; i++)
+    {
+        // -61(`) 빼기
+        char cbuf = temp[i];
+        int ibuf = (int)(cbuf - '`');
+        if(alphacount[ibuf] == -1)
+        {
+            alphacount[ibuf] = i;
+        }
+        else
+        {
+            continue;
+        }
+        
+    }
+    
+    for(int j = 0; j < 26; j++)
+    {
+        printf("%d ",alphacount[j]);
+    }
+}
