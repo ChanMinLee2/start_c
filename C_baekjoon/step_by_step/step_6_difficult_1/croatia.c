b084a7ee5c39d860e2424a1b781104b97688c0af
chanmin Lee <dlcksals87@gmail.com>
2023-10-10 00:06:36 +0900

    croatia alphabet checking problem success

--- C_baekjoon/step_by_step/step_6_difficult_1/croatia.c
+++ C_baekjoon/step_by_step/step_6_difficult_1/croatia.c
@@ -3,70 +3,56 @@
 // 2차 시도 실패 - 80퍼 시간초과 , 일부 알파벳 조건 이상했음(d= 없는데 --해버림, dz== 오류)
 // 하지만 수정후에도 실패함. 시간초과되는 이유 찾아볼 것. 
 
+// 3차 시도 성공(구글링) - 이유 탐색 , getchar로 풀면 시간초과인듯하여 문자열 채로 입력받아 index로 조절하는 방식 채택. 
+
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
     int alphacount = 0;
+    char input[101];
     char buf; 
     char before_buf;
     
-    while((buf = getchar()) != '\n')
+    scanf("%s", input);
+    
+    for(int i = 0; i < strlen(input); i++)
     {
         alphacount++;
         
-        printf("%d : %c\n", alphacount,buf);
-        
-        if(before_buf == 'c' || before_buf == 'd')
+        if(input[i] == '=')
         {
-            if(buf == '-')
+            if(input[i-2] == 'd' && input[i-1] == 'z')
             {
-                alphacount--;
-                before_buf = buf;
+                alphacount -= 2;
                 continue;
             }
             
-            else if(before_buf == 'c' && buf == '=')
+            else if(input[i-1] == 's' || input[i-1] == 'z' || input[i-1] =='c')
             {
                 alphacount--;
-                before_buf = buf;
-                continue;
-            }
-            
-            else if(before_buf == 'd' && buf == 'z')
-            {
-                before_buf = buf;
-                buf = getchar();
-                alphacount++;
-                if(buf == '=')
-                {
-                    alphacount = alphacount - 2;
-                    before_buf = buf;
-                    continue;
-                }
-            }
-        }
-        
-        else if (before_buf == 'l' || before_buf == 'n')
-        {
-            if(buf == 'j')
-            {
-                alphacount--;
-                before_buf = buf;
                 continue;
             }
         }
         
-        else if( before_buf == 's' || before_buf == 'z')
+        else if(input[i] == '-')
         {
-            if(buf == '=')
+            if(input[i-1] == 'd' || input[i-1] == 'c')
             {
                 alphacount--;
-                before_buf = buf;
                 continue;
             }
         }
-        before_buf = buf;
+        
+        else if(input[i] == 'j')
+        {
+            if(input[i-1] == 'n' || input[i-1] == 'l')
+            {
+                alphacount--;
+                continue;
+            }
+        }
     }
 
     printf("%d", alphacount);
