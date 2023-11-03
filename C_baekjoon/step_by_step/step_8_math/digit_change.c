6eee176649a89d22c5dee15c0aa98de99083bc19
chanmin Lee <dlcksals87@gmail.com>
2023-11-04 00:15:43 +0900

    digit change problem success

--- C_baekjoon/step_by_step/step_8_math/digit_change.c
+++ C_baekjoon/step_by_step/step_8_math/digit_change.c
@@ -1,8 +1,9 @@
 // 1차 시도 실패 : B자리 수를 10진수로 변환하는 과정에서 carry를 생각하지 않았음... 어떡할까
+// 2차 시도 성공 : 구조적으로 다 고침, 굳이 배열과 함수가 필요 하지 않음, 또한 포인터변수로만은 입력받기가 안되므로 배열변수로 바꿈. 
+// 그리고 digitchangednum 구할때 pow조건변경. 
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
-
 
 int main()
 {
@@ -11,26 +12,22 @@ int main()
     int digit_changed_num = 0;
     int numlen= 0;
     
-    int ascii_alpha_to_num[26];
-    
-    for (int i = 0; i < 26; i++) {
-        ascii_alpha_to_num[i] = 10 + i;
-        //printf("%d , ", ascii_alpha_to_num[i]);
-    }
-    
     scanf("%s %d", num, &digit);
-    printf("%s %d\n", num, digit);
+    //printf("%s %d\n", num, digit);
     
     numlen = strlen(num);
     for (int i = numlen-1; i >= 0; i--) {
-        if(*(num+i) >= '0' && *(num+i) <='9')
+        if(num[i] >= '0' && num[i] <='9')
         {
-            digit_changed_num += num[i] * pow(10, i);
+            digit_changed_num += (num[i]-'0') * pow(digit, numlen-1-i);
+            printf("%d good, num : %d\n", i, digit_changed_num);
+            
         }
             
         else
         {
-            digit_changed_num += ascii_alpha_to_num[(int)(num[i]-'A')] * pow(10, i);
+            digit_changed_num += ((num[i]-'A')+10) * pow(digit, numlen-1-i);
+            printf("%d good, num : %d\n", i, digit_changed_num);
         }
             
     }
