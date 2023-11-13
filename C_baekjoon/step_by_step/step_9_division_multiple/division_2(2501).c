dacc58471fb1cb560a0b8a36988980b6fc5386c1
chanmin Lee <dlcksals87@gmail.com>
2023-11-13 18:35:12 +0900

    division problem 2 2nd attempt

--- C_baekjoon/step_by_step/step_9_division_multiple/division_2(2501).c
+++ C_baekjoon/step_by_step/step_9_division_multiple/division_2(2501).c
@@ -1,4 +1,6 @@
 // 1차 시도 실패 : 22퍼 부근에서 안됨. 아마 출력 조건이 이상한듯.
+// 2차 시도 실패 : 출력 조건 order와 idx 비교문 추가. 만약 들어온 order가 idx보다 크면 접근이 불가하므로 0출력해야함. 근데 또틀림. 
+
 
 #include <stdio.h>
 
@@ -20,5 +22,16 @@ int main()
         }
     }
     
-    printf("%d", division[order-1]);
+    
+    if(order-1 > division_idx-1)
+    {
+        printf("%d", 0);
+    }
+    
+    else{
+        printf("%d", division[order-1]);
+    }
+    
+}
+
 }
