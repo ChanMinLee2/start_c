a19257a78e385560e4fc84123447411a160267e9
chanmin Lee <dlcksals87@gmail.com>
2023-11-16 19:24:06 +0900

    division 2 problem success

--- C_baekjoon/step_by_step/step_9_division_multiple/division_2(2501).c
+++ C_baekjoon/step_by_step/step_9_division_multiple/division_2(2501).c
@@ -1,6 +1,6 @@
 // 1차 시도 실패 : 22퍼 부근에서 안됨. 아마 출력 조건이 이상한듯.
 // 2차 시도 실패 : 출력 조건 order와 idx 비교문 추가. 만약 들어온 order가 idx보다 크면 접근이 불가하므로 0출력해야함. 근데 또틀림. 
-
+// 3차 시도 성공 : 반복문에서 시간 아낄려고 input/2했는데, 오히려 1 1 케이스에서 반복문이 바로 끝나는 경우 발생. 수정. 
 
 #include <stdio.h>
 
@@ -14,7 +14,7 @@ int main()
     
     scanf("%d %d", &input, &order);
     
-    for(int i = 1; i<=input/2; i++)
+    for(int i = 1; i<=input; i++)
     {
         if(input % i == 0)
         {
@@ -34,4 +34,3 @@ int main()
     
 }
 
-}
