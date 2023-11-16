e9bb7411ce176f49b6b346bc8eae536b8a1d0151
chanmin Lee <dlcksals87@gmail.com>
2023-11-17 01:05:34 +0900

    perfect number checking problem success

--- C_baekjoon/step_by_step/step_9_division_multiple/perfect_num(9506).c
+++ C_baekjoon/step_by_step/step_9_division_multiple/perfect_num(9506).c
@@ -1,16 +1,19 @@
 // 1차 시도 실패 : 16퍼센트 실패, 조건 중 문제 있는지 다시 체크중...
+// 3차 시도 성공 : 오류 1 - div_idx, sum을 루프마다 초기화하지 않았음, 오류 2 - 출력 시 줄바꿈과 띄어쓰기 맞추지 않았음 - 수정 후 성공
 #include <stdio.h>
 
 int main()
 {
     int input = 0; 
-    int divisions[50000];
+    int divisions[100000];
     int divisions_idx = 0;
     int sum_of_divs = 0;
     
-    
     while(1)
     {
+        divisions_idx = 0;
+        sum_of_divs = 0;
+        
         scanf("%d",&input);
         if(input == -1)
             break;
@@ -21,6 +24,7 @@ int main()
             {
                 divisions[divisions_idx++] = i;
                 sum_of_divs += i;
+                //printf("%d %d\n", i ,sum_of_divs);
             }
         }
         
@@ -29,16 +33,22 @@ int main()
             printf("%d = ", input);
             for (int i = 0; i < divisions_idx; i++) {
                 if(i == 0)
-                    printf("%d ", divisions[i]);
+                    printf("%d", divisions[i]);
+                else if(i == divisions_idx-1)
+                {
+                    printf(" + %d\n", divisions[i]);
+                }
                 else
-                    printf("+ %d ", divisions[i]);
+                    printf(" + %d", divisions[i]);
+                    
             }
         }
         
         else{
-            printf("%d is NOT perfect.", input);
+            printf("%d is NOT perfect.\n", input);
         }
         
     }
     
 }
+
