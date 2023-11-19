9e0dddadcee92f4fd413d8e8045867aa9a5d3bcd
chanmin Lee <dlcksals87@gmail.com>
2023-11-19 11:14:04 +0900

    prime numbers sum in range problem 1st attempt

---
+++ C_baekjoon/step_by_step/step_9_division_multiple/prime_sum_in_range(2581).c
@@ -0,0 +1,55 @@
+// 1차 시도 실패 : 3퍼센트 실패 반례 찾아야 함... 
+#include <stdio.h>
+
+int main()
+{
+    int start, end = 0;
+    
+    scanf("%d", &start);
+    scanf("%d", &end);
+    
+    int sum, min = 0;
+    
+    for (int i = start; i < end+1; i++) {
+        if(i == 1)
+        {
+            continue;
+        }
+        else if( i == 2)
+        {
+            sum += i;
+            min = i;
+            continue;
+        }
+        int prime_check = 1;
+        for (int j = 2; j < i; j++) {
+            if(i % j == 0)
+            {
+                prime_check = 0;
+                break;
+            }
+        }
+        if(prime_check == 1)
+        {
+            if(sum == 0)
+            {
+                sum += i;
+                min = i;
+            }
+            else{
+                sum += i;
+            }
+        }
+    }
+    
+    if( sum == 0 )
+    {
+        sum = -1;
+        printf("%d", sum);
+    }
+    
+    else{
+        printf("%d \n%d", sum, min);
+    }
+    
+}
