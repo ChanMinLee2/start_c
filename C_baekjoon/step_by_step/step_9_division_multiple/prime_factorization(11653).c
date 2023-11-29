d84bb058d31b24a9ca3384a6f358df18e95a1127
chanmin Lee <dlcksals87@gmail.com>
2023-11-29 13:55:42 +0900

    prime factorization problem 2nd attempt

--- C_baekjoon/step_by_step/step_9_division_multiple/prime_factorization(11653).c
+++ C_baekjoon/step_by_step/step_9_division_multiple/prime_factorization(11653).c
@@ -1,67 +1,34 @@
+// 1차 시도 실패 : 시간초과 이유? 아마도 for 루프 때문일듯? 매번 나눌때마다 소수확인하는게 문제인듯. 
+// 2차 시도 실패 : ㅋㅋㅋ 소인수분해를 이해하지 못했음. 그냥 작은 수부터 나눌수있는 만큼 나누면 소수가 아닌 수로는 안나눠지는구나. 
+//                  아예 새로 만들었으나, 97퍼에서 실패함. 아마 특수 케이스 무언가를 생각 안한듯. 
 
 #include <stdio.h>
-#include <stdlib.h>
-
-int check_prime(int num)
-{
-    if(num == 1)
-    {
-        return 0;
-    }
-    
-    if(num == 2){
-        return 1;
-    }
-    
-    else
-    {
-        for (int i = 2; i < num; i++) {
-            if(num % i == 0)
-            {
-                return 0;    
-            }
-        }
-        
-        // for루프 탈출 = 나눠지는 수가 없음. 
-        return 1;
-    }
-}
 
 int main()
 {
     int num = 0;
     
-    int* primes;
-    int primes_size = 0;
     scanf("%d", &num);
     
-    // num보다 작은 소수들의 동적배열을 생성 
-    for (int i = 2; i <= num; i++) {
-        if( check_prime(i) ==  1)
-        {
-            primes = (int*)malloc(sizeof(int));
-            primes[primes_size++] = i;
-        }
-    }
-    
-    // num을 소수들의 동적배열의 원소로 가능한 만큼 나누는 걸 반복. 
-    int wh_idx = 0;
-    while(wh_idx < primes_size)
+    if( num == 1 )
     {
-        // 나눌수 있다면 나누고
-        if( num % primes[wh_idx] == 0) 
-        {
-            num / primes[wh_idx];
-            printf("%d\n", primes[wh_idx]);
-        }
-        
-        // 나눌수 없다면 다음 소수로 나눌 준비 
-        else
-        {
-            wh_idx++;
-            continue;
-        }
+        return 0;
     }
     
     
+    int div_num = num;
+    for (int i = 2; i <= num/2; i++) {
+        if(div_num % i == 0)
+        {
+            div_num /= i;
+            printf("%d\n", i--);
+        }
+    }
+    
+    if(div_num == num )
+    {
+        printf("%d", num);
+    }
 }
+
+
