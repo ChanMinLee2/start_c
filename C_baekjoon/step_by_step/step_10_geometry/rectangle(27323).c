4a1abd84a1095ac8bc9b97594224501073e3c03a
chanmin Lee <dlcksals87@gmail.com>
2023-12-01 00:23:44 +0900

    rectangle problem success

---
+++ C_baekjoon/step_by_step/step_10_geometry/rectangle(27323).c
@@ -0,0 +1,14 @@
+// 1차 시도 성공 
+
+#include <stdio.h>
+
+int main(void)
+{
+    int side_a = 0;
+    int side_b = 0;
+    
+    scanf("%d", &side_a);
+    scanf("%d", &side_b);
+    
+    printf("%d", side_a * side_b);
+}
