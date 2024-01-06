e3836c057befc3f177f64d943f8c2e3eaa00fbc4
chanmin Lee <dlcksals87@gmail.com>
2024-01-06 10:59:41 +0900

    chess problem 1st attempt

--- C_baekjoon/step_by_step/step_12_brute_force/chess.c
+++ C_baekjoon/step_by_step/step_12_brute_force/chess.c
@@ -1,22 +1,63 @@
-// 1차 시도 : 방향성 잡기... 너무 어렵다. 
+// 1차 시도 실패 : 전혀 다른 예측값이 나옴. chess와 input 비교 로직 문제인듯
+
 #include <stdio.h>
 
 int main()
 {
-    int m, n;
-    scanf("%d %d", &m, &n);
+    char chess1[8][8] = {{"WBWBWBWB"}, {"WBWBWBWB"}, {"WBWBWBWB"},{"WBWBWBWB"},{"WBWBWBWB"},{"WBWBWBWB"},{"WBWBWBWB"},{"WBWBWBWB"}};
+    char chess2[8][8] = {{"BWBWBWBW"}, {"BWBWBWBW"},{"BWBWBWBW"},{"BWBWBWBW"},{"BWBWBWBW"},{"BWBWBWBW"},{"BWBWBWBW"},{"BWBWBWBW"}};
     
-    char chess_input[50][50];
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            scanf("%c", &chess_input[i][j]);
+    char input[50][50];
+    
+    int m,n = 0;
+    
+    scanf("%d %d", &n, &m);
+    
+    for (int i = 0; i < n; i++) {
+        scanf("%s", &input[i][0]);
+    }
+    
+    int min_chess = 0;
+    
+    for (int i = 0; i < n-7; i++) {
+        for (int j = 0; j < m-7; j++) {
+            int min_guess1 = 0;
+            int min_guess2 = 0;
+            for (int k = i; k < i+8; k++) {
+                for (int l = j; l < j+8; l++) {
+                    // chess1
+                    if(input[k][l] != chess1[k][l])
+                    {
+                        min_guess1++;
+                    }
+                    
+                    // chess2
+                    if(input[k][l] != chess2[k][l])
+                    {
+                        min_guess2++;
+                    }
+                }
+            }
+            
+            if(i == 0 && j == 0)
+            {
+                (min_guess1 < min_guess2) ? (min_chess = min_guess1) : (min_chess = min_guess2);
+                //printf("%d %d", min_guess1, min_guess2);
+                continue;
+            }
+            
+            if(min_guess1 < min_guess2 && min_guess1 < min_chess)
+            {
+                min_chess = min_guess1;
+            }
+            
+            else if(min_guess2 < min_guess1 && min_guess2 < min_chess)
+            {
+                min_chess = min_guess2;
+            }
+            
         }
     }
     
-    // case 1 - 시작블록이 B인경우랑 비교
-    for (int i = 0; i < m-8; i++) {
-        
-    }
-
-    // case 2 - 시작블록이 w인경우랑 비교 
+    printf("%d", min_chess);
 }
