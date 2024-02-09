972322a8266d6d46eb9a3d283bee15299680ccea
chanmin Lee <dlcksals87@gmail.com>
2024-02-10 00:24:51 +0900

    num_card problem success

--- C_baekjoon/step_by_step/step_14_set&map/num_card.c
+++ C_baekjoon/step_by_step/step_14_set&map/num_card.c
@@ -1,4 +1,5 @@
 // 1차시도 실패 : 시간초과 - set의 사용 필요 
+// 2차시도 성공 : 질문게시판 - 이진탐색 확인. 이진탐색 중 인덱스 범위 벗어나는 경우(s > e) 확인 및 수정 후 성공
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -6,30 +7,86 @@ int my_cards[500000];
 int given_cards[500000];
 int result[500000];
 
-int static compare (const void* first, const void* second)
+void merge(int * arr, int s, int m, int e)
 {
-    if (*(int*)first > *(int*)second)
-        return 1;
-    else if (*(int*)first < *(int*)second)
-        return -1;
-    else
-        return 0;
+    // s : start point, m : mid point, e : end point
+
+    int n1 = m - s + 1; // left array's size
+    int n2 = e - m;     // right array's size
+    int left_arr[n1+1]; 
+    int right_arr[n2+1];
+
+    // first for loop - copy arr values to left
+    for (int i = 0; i < n1; i++) 
+    {
+        left_arr[i] = arr[s + i];
+    }
+    
+    // second for loop - copy arr values to right
+    for (int j = 0; j < n2; j++)
+    {
+        right_arr[j] = arr[m + j + 1];
+    }
+
+    left_arr[n1] = 2147483647; // 정수형 최댓값 ()
+    right_arr[n2] = 2147483647;
+
+    int idx_left = 0; 
+    int idx_right = 0; 
+
+    for (int k = s; k <= e; k++)
+    {
+        if (left_arr[idx_left] <= right_arr[idx_right])
+        {
+            arr[k] = left_arr[idx_left];
+            idx_left++;
+        }
+
+        else
+        {
+            arr[k] = right_arr[idx_right];
+            idx_right++;
+        }
+    }
+}
+
+void merge_sort(int * arr, int s, int e)
+{
+    if( s < e )
+    {
+        int m = (s+e)/2;
+        merge_sort(arr, s, m);
+        merge_sort(arr, m+1, e);
+        merge(arr,s,m,e);
+    }
 }
 
 // 배열은 첫 자리(이름변수) 이후로 연결된 리스트이기 떄문에 단일 포인터로 다른 함수에서 변경 가능하다.
 int binary_search(int* arr, int key, int s, int e)
 {
-    int m = s + e / 2;
-    if (arr[m] > key)
+    if(s >= e)
     {
-        binary_search(arr, key, m+1, e);
+        if(arr[s] == key)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
     }
-    else if ( arr[m] < key)
+    
+    int m = (s + e)/2;
+    if(arr[m] == key)
     {
-        binary_search(arr, key, s, m-1);
+        return 1;
+    }
+    else if (arr[m] > key)
+    {
+        return binary_search(arr, key, s, m-1);
     }
     else{
-        return 1;
+        return binary_search(arr, key, m+1, e);
     }
 }
 
@@ -49,18 +106,17 @@ int main()
         scanf("%d", &given_cards[i]);
     }
 
-    qsort(my_cards, my_count, sizeof(int), compare);
+    merge_sort(my_cards, 0, my_count-1);
     
     for (int i = 0; i < given_count; i++) {
         if (binary_search(my_cards, given_cards[i], 0, my_count-1) == 1)
         {
             result[i]++;
         }
-        
     }
     
     for (int i = 0; i < given_count; i++) {
-        printf("%d", result[i]);
+        printf("%d ", result[i]);
     }
     
 }
