6d92982fc72a3c55d84773b006eb29007a76bc27
chanmin Lee <dlcksals87@gmail.com>
2024-07-20 23:55:00 +0900

    🎨 deq2 problem direction setting

---
+++ C_baekjoon/step_by_step/step_16_stack_queue/deq2.c
@@ -0,0 +1,16 @@
+// deq의 기본 문제
+#include <stdio.h>
+
+int deq[1000000];
+int front = 0;
+int rear = 0;
+
+void deq_front_push(int add_num);
+void deq_rear_push(int add_num);
+int deq_front_pop();
+int deq_rear_pop();
+
+int main()
+{
+    
+}
