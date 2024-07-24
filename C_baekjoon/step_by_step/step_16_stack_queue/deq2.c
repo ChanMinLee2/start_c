c304efce1b2600d6fb31bb357e93d44017c4c386
ChanMinLee2 <dlcksals87@gmail.com>
2024-07-24 22:29:03 +0900

    🎨 deq2 problem skeleton setting

--- C_baekjoon/step_by_step/step_16_stack_queue/deq2.c
+++ C_baekjoon/step_by_step/step_16_stack_queue/deq2.c
@@ -9,8 +9,55 @@ void deq_front_push(int add_num);
 void deq_rear_push(int add_num);
 int deq_front_pop();
 int deq_rear_pop();
+int deq_count();
+int deq_empth_check();
+int deq_print_front();
+int deq_print_rear();
 
 int main()
 {
-    
+    int count = 0;
+    int order_type = 0;
+
+    for (int i = 0; i < count; i++)
+    {
+      scanf("%d", &order_type);
+      switch (order_type)
+      {
+      case 1:
+        /* code */
+        break;
+
+      case 2:
+        /* code */
+        break;
+
+      case 3:
+        /* code */
+        break;
+
+      case 4:
+        /* code */
+        break;
+
+      case 5:
+        /* code */
+        break;
+
+      case 6:
+        /* code */
+        break;
+
+      case 7:
+        /* code */
+        break;
+
+      case 8:
+        /* code */
+        break;
+
+      default:
+        break;
+      }
+    }
 }
